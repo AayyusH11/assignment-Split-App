74136b03414eb76b298a746b85d406791b313aa4
Ashutosh Singh <si.ayush7711@gmail.com>
2025-12-19 19:23:28 +0530

    Update App.jsx for adding a logout button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,16 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
 
+  const logout=()=>{
+    setCurrentUser(null);   
+  };
+
+
   if (!currentUser) {
     return <Login onSelectUser={setCurrentUser} />;
   }
 
-  return <Dashboard user={currentUser} />;
+  return <Dashboard user={currentUser} onLogout={logout} />;
 }
 
 export default App;
