eb5e5184b0af5e5a311f3f9fe6bf13479f55799e
Ashutosh Singh <si.ayush7711@gmail.com>
2025-12-20 13:42:09 +0530

    Update balance.controller.js bug

--- backend/src/controllers/balance.controller.js
+++ backend/src/controllers/balance.controller.js
@@ -16,23 +16,5 @@ const getBalances = async (req, res) => {
 };
 
 
-const settlePartialBalance = async (req, res) => {
-  const { from, to, amount } = req.body;
-
-  const balance = await Balance.findOne({ from, to });
-
-  if (!balance) {
-    return res.status(404).json({ error: "No such balance exists" });
-  }
-
-  if (amount >= balance.amount) {
-    await Balance.deleteOne({ _id: balance._id });
-  } else {
-    balance.amount -= amount;
-    await balance.save();
-  }
-
-  res.json({ message: "Balance settled successfully" });
-};
 
 module.exports = {getBalances,settlePartialBalance};
