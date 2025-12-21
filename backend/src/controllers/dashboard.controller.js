256331ef4f5040a7c51edc9ac63d67682dda3d1d
Ashutosh Singh <si.ayush7711@gmail.com>
2025-12-21 17:49:30 +0530

    Update dashboard.controller.js fixed the issue of balances in dashboard
    if two users owe each other same amount this must be cancelled 
    implemented this here

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -4,39 +4,71 @@ const User = require("../models/User");
 
 const round2 = (num) => Math.round(num * 100) / 100;
 
+
+
+
 const getDashboard = async (req, res) => {
   try {
     const { userId } = req.params;
     const userObjectId = new mongoose.Types.ObjectId(userId);
 
-    //  Money that i owe (Payables)
-    const youOweRaw = await Balance.aggregate([
-      { $match: { from: userObjectId } },
-      {
-        $group: {
-          _id: "$to",
-          amount: { $sum: "$amount" },
-        },
-      },
-    ]);
+//in dashboard mutual settlement (when two users owe each other the same ammount that is not working
+//change in logic required here 
+//simply accumulating wont workout here
 
-    //  Money that is owed to me(recievables)
-    const owedToYouRaw = await Balance.aggregate([
-      { $match: { to: userObjectId } },
-      {
-        $group: {
-          _id: "$from",
-          amount: { $sum: "$amount" },
-        },
-      },
-    ]);
+   
 
-    // Collecting all the involved user IDs
-    const userIds = [
-      ...youOweRaw.map(item => item._id),
-      ...owedToYouRaw.map(item => item._id),
-    ];
+    const balances=await Balance.find({
+      $or:[
+        {from:userObjectId},
+        {to:userObjectId},
+      ],
+    });
 
+    const net={};
+
+    balances.forEach(b=>{
+      const from=b.from.toString();
+      const to=b.to.toString();
+      const amount=round2(Number(b.amount));
+      
+      
+      if(from===userId){
+        // i(logged in user) owe someone
+        net[to]=round2((net[to] || 0)-amount);
+      }
+
+      if(to===userId){
+        // someone owes me
+        net[from]=round2((net[from]||0)+amount);
+      }
+    
+    });
+
+    // now can split into payeables and recievables 
+   const youOwe =[];
+    const owedToYou=[];
+
+    Object.entries(net).forEach(([otherUserId,amount])=>{
+
+      if(amount<0){
+        youOwe.push({
+          _id:otherUserId,
+          amount:round2(Math.abs(amount)),
+        });
+      }
+      else if(amount>0){
+        owedToYou.push({
+          _id:otherUserId,
+          amount:round2(amount),
+
+        });
+      }
+    });
+
+    // mapping needed old approach will fail 
+
+    const userIds = [...youOwe, ...owedToYou].map(u => u._id);
     // Fetching the user names
     const users = await User.find({ _id: { $in: userIds } });
 
@@ -45,29 +77,23 @@ const getDashboard = async (req, res) => {
       userMap[u._id.toString()] = u.name;
     });
 
-    //  Attach names
-    const youOwe = youOweRaw.map(item => ({
-      _id: item._id,
-      name: userMap[item._id.toString()] || "Unknown",
-      amount: item.amount,
-    }));
+    //Now will attach names for each section 
+    youOwe.forEach(i => {
+      i.name = userMap[i._id] || "Unknown";
+    });
 
-    const owedToYou = owedToYouRaw.map(item => ({
-      _id: item._id,
-      name: userMap[item._id.toString()] || "Unknown",
-      amount: item.amount,
-    }));
+    owedToYou.forEach(i => {
+      i.name = userMap[i._id] || "Unknown";
+    });
 
-    // Now Total
-    
-
+    // Totals
     const totalYouOwe = round2(
-     youOwe.reduce((sum, x) => sum + x.amount, 0)
-     );
+      youOwe.reduce((sum, x) => sum + x.amount, 0)
+    );
 
     const totalOwedToYou = round2(
       owedToYou.reduce((sum, x) => sum + x.amount, 0)
-     ); 
+    );
 
     res.json({
       youOwe,
